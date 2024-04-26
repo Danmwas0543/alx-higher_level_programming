665da39d606189dd5682d054591cedf4eb134948
daniel <danielmwangi12244@gmail.com>
2024-04-25 21:27:32 -0700

    printing uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     for iterator in str:
         tp = iterator
         if ord(tp) >= 97 and ord(tp) <= 122:
-            tp = chr(ord(iterator) -32)
+            tp = chr(ord(iterator) - 32)
         print("{}".format(tp), end='')
     print()

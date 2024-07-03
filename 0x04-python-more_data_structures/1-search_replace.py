b94e0156d9c8144091e9d07d483ef94af2aaf816
Daniel <danielmwangi12244@gmail.com>
2024-07-03 05:44:19 -0700

    data structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,8 @@
+#!/usr/bin/python3
 
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for indx, itms in enumerate (new_list):
+        if itms == search:
+            new_list[indx] = replace
+    return new_list

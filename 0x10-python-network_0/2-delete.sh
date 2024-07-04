1a089acc091338412281257dd955be3c72fbd86d
Daniel <danielmwangi12244@gmail.com>
2024-07-04 16:32:13 -0700

    python networks

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #a script sending a delete request to a url and displays the  response's body
-curl -sx "DELETE" "$1"
+curl -sX "DELETE" "$1"
---
+++ 0x10-python-network_0/3-methods.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+#a script which takes a url and displays all methods the server will accept

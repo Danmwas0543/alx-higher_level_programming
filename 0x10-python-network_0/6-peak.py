9fe1e26704110a0c49ae7d758beb7baf42962acf
Daniel <danielmwangi12244@gmail.com>
2024-07-04 18:10:37 -0700

    python networks

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,6 +2,7 @@
 """ a function that finds a peak in a list of unsorted integers. """
 
 
+
 def find_peak(list_of_integers):
     """ finding the shortest algorithm """
     if list_of_integers == []:

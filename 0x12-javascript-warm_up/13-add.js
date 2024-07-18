e86d09f68c0f5d2e6841b2ff303da9b9e6fb6efe
Daniel <danielmwangi12244@gmail.com>
2024-07-18 01:15:39 -0700

    javascript-warm_up

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/*
+ * This function exports a function named 'add' that returns the addition of two integers.
+ */
+exports.mugo = function dan(a, faith) {
+  return a + faith;
+};

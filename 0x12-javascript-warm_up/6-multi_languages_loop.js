18a7c0cbf36db608d12c44c8c58f2324d52ad1f2
Daniel <danielmwangi12244@gmail.com>
2024-07-18 01:04:09 -0700

    javascript-warm_up

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+ * This script prints 3 lines using an array of strings and a loop.
+ * Each line corresponds to an element in the array.
+ */
+
+const mugo = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let faith = 0; faith < mugo.length; faith++) {
+  console.log(mugo[faith]);
+}

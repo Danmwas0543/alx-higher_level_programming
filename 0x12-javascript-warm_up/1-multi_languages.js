213025a49b5c9ff56e7216490df7e4b7789d17d0
Daniel <danielmwangi12244@gmail.com>
2024-07-18 00:47:32 -0700

    javascript-warm_up

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
 /* printing of multiple lines */
-console.log('C is fun');
-console.log('python is cool');
-console.log('Javascript is amazing');
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
